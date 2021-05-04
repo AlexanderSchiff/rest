b1430ab8df6367d07f6d88296080ed638495d392
AlexanderSchiff <alex.schiff@gmail.com>
2021-05-04 14:11:44 +0000

    more functionality
    Signed-off-by: AlexanderSchiff <alex.schiff@gmail.com>

--- controllers/pet_controller.go
+++ controllers/pet_controller.go
@@ -66,7 +66,7 @@ func (pC PetController) FindPetsByStatus(context *gin.Context) {
 	}
 
 	pets, err := pC.PetService.GetByStatus(status)
-		if err != nil {
+	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -77,6 +77,15 @@ func (pC PetController) FindPetsByStatus(context *gin.Context) {
 // FindPetsByTags finds pets by tags
 func (pC PetController) FindPetsByTags(context *gin.Context) {
 	// find by tags. need to bind to multiple query params
+	params := context.Request.URL.Query()
+	tags := params["tags"]
+	pets, err := pC.PetService.GetByTags(tags)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"data": pets})
 }
 
 // GetPetByID gets a pet by ID
@@ -98,7 +107,25 @@ func (pC PetController) GetPetByID(context *gin.Context) {
 
 // UpdatePetWithForm updates a pet using a form
 func (pC PetController) UpdatePetWithForm(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("petID"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	status := context.Query("status")
+	if status != "available" && status != "pending" && status != "sold" {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "invalid status"})
+		return
+	}
+	
+	name := context.Query("name")
+	pet, err := pC.PetService.Update(models.Pet{ID: id, Name: name, Status: models.PetStatus(status)})
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	context.JSON(http.StatusOK, gin.H{"data": pet})
 }
 
 // DeletePet deletes a pet
